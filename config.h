9ae2ee5dbacf3eec4179ecb4b246bbd30c287cfc
liu.bian.shi <liu.bian.shi@gmail.com>
2020-08-14 16:51:48 +0800

    调整字体

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static int topbar = 1;  /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1;   /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-    "SauceCodePro Nerd Font Mono:pixelsize=28:antialias=true:autohint:false:hint:ture:hintstyle:hintfull",
+    "FiraCode Nerd Font Mono:pixelsize=28:antialias=true:autohint:false:hint:ture:hintstyle:hintfull",
     "WenQuanYi Micro Hei:pixelsize=28:antialias=true:autohint:false:hint:ture:hintstyle:hintfull",
     "Noto Color Emoji:pixelsize=28:antialias=true:autohint:false:hint:ture:hintstyle:hintfull",
     "JoyPixels:pixelsize=28:antialias=true:autohint=true",
